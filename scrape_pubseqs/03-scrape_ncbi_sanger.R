a811a780d14663888d31fb3ccdd6a8fa0d176ed4
nickbrazeau <nbrazeau@med.unc.edu>
2019-09-03 12:37:25 -0400

    update sanger seq

--- scrape_pubseqs/03-scrape_ncbi_sanger.R
+++ scrape_pubseqs/03-scrape_ncbi_sanger.R
@@ -1,9 +1,10 @@
-ncbiapikey <- readr::read_tsv("~/Documents/MountPoints/mountedMeshnick/Projects/VivID_Seq/scrape_pubseqs/ncbi_api_key_forsanger.txt", col_names = F)
+setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Seq/scrape_pubseqs")
+ncbiapikey <- readr::read_tsv("/ncbi_api_key_forsanger.txt", col_names = F)
 set_entrez_key(unname(unlist(ncbiapikey)))
 library(tidyverse) 
 library(rentrez)
 
-mtsanger <- readxl::read_excel("~/Documents/MountPoints/mountedMeshnick/Projects/VivID_Seq/scrape_pubseqs/vivid_seq_public_sangerseq.xlsx")
+mtsanger <- readxl::read_excel("vivid_seq_public_sangerseq.xlsx")
 
 
 ## Query
@@ -25,44 +26,24 @@ seqacc$id <- lapply(search, function(x){x$ids})
 ## Scrape/Fetch
 # https://stackoverflow.com/questions/12193779/how-to-write-trycatch-in-r
 fasta_fetch <- function(id){
-  out <- tryCatch(
-    {
-      rentrez::entrez_fetch(db="nucleotide", rettype="fasta", id = id)
-      Sys.sleep(10)
-    },
-    error=function(cond) {
-      message(paste("ID does not seem to exist:", id))
-      message("Here's the original error message:")
-      message(cond)
-      # Choose a return value in case of error
-      return(NA)
-    },
-    warning=function(cond) {
-      message(paste("ID caused a warning:", id))
-      message("Here's the original warning message:")
-      message(cond)
-      # Choose a return value in case of warning
-      return(NULL)
-    },
-    finally={
-      message(paste("Processed id:", id))
-    }
-  )    
-  return(out)
+  ret <- rentrez::entrez_fetch(db="nucleotide", rettype="fasta", id = id)
+  Sys.sleep(60)
+  return(ret)
 }
   
 
+seqacc$fasta <- NA
+for(i in 1:nrow(seqacc)){
+  seqacc$fasta[i] <- fasta_fetch(seqacc$id[i])
+}
 
 
-seqacc$fasta <- purrr::map(seqacc$id, fasta_fetch)
-
-
+saveRDS(seqacc, "/pine/scr/n/f/nfb/Projects/VivID_Seq/public_sanger_seqs/global_sangerseq_seqacc.rds")
 
 outfastas <- purrr::map(seqacc, `[[`, "fasta")
-dir.create("~/Documents/MountPoints/mountedScratchLL/Projects/VivID_Seq/vividseq_sanger_publicseqs/",
-           recursive = T)
-write.table(x=outfastas, file="~/Documents/MountPoints/mountedScratchLL/Projects/VivID_Seq/vividseq_sanger_publicseqs/vividseq_sanger_publicseqs.fa", 
-            quote = F, col.names = F, row.names = F, sep = "")
+seqinr::write.fasta(sequences = outfastas,
+                    names = seqacc$acc,
+                    file.out = "/pine/scr/n/f/nfb/Projects/VivID_Seq/public_sanger_seqs/global_sangerseq_vivid.fasta")
 
 
 

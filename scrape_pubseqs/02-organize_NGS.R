87ccd60113ec581ab4ce4206ba75cd403fd3481a
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-09-03 16:39:45 +0100

    organize pe for runmaps

--- scrape_pubseqs/02-organize_NGS.R
+++ scrape_pubseqs/02-organize_NGS.R
@@ -9,27 +9,47 @@ pe.long <- pe %>%
   dplyr::mutate(SRR1 = basename(R1),
                 SRR2 = basename(R2)) %>% 
   dplyr::select(-c("R1", "R2")) %>% 
-  tidyr::gather(., key = "srr", value = "fastq") %>% 
+  tidyr::gather(., key = "srr", value = "fastq", 2:3) %>% 
   dplyr::select(-c("srr"))
 
-pe.paths <- tibble::tibble(path = list.files(path = "",
+pe.paths <- tibble::tibble(path = list.files(path = "~/Documents/MountPoints/mountedScratchLL/Projects/VivID_Seq/public_pe_seqs/",
                                               pattern = "fastq.gz",
                                               full.names = T)) %>% 
   dplyr::mutate(fastq = basename(path))
 
+
+pe.long.paths <- dplyr::left_join(pe.long, pe.paths, by = "fastq")
+
+
 #...............................
 # Make Symlink Architecture 
 #................................
-
+symlink_architecture <- pe.long.paths %>% 
+  magrittr::set_colnames(c("smpl", "fastq", "from")) %>% 
+  dplyr::mutate(to = paste0(smpl, "/", fastq)) %>% 
+  dplyr::select(-c("fastq")) %>% 
+  dplyr::mutate(from = gsub("/Users/nickbrazeau/Documents/MountPoints/mountedScratchLL/", 
+                            "/proj/ideel/meshnick/users/NickB/", 
+                            from))
+    
+readr::write_tsv(x = symlink_architecture, 
+                 path = "~/Documents/MountPoints/mountedMeshnick/Projects/VivID_Seq/wgs_pe_improved_global/symlink_architecture.tab.txt",
+                 col_names = F)  
 
 #...............................
 # Make Run Map
 #................................
+globalvivid_run_map <- pe.long.paths %>% 
+  dplyr::select(-c("path")) %>% 
+  dplyr::mutate(fastq = stringr::str_split_fixed(fastq, "_", n=2)[,1]) %>% 
+  dplyr::filter(!duplicated(.)) %>% 
+  dplyr::mutate(x = ".")
 
-pe.run.map <- dplyr::left_join(pe.long, pe.paths) %>% 
-  dplyr::mutate(read = ifelse(stringr::str_detect(fastq, "_1.fastq.gz"), "R1", "R2")
-                ) %>% 
-  tidyr::spread(., key = "read", value = "path")
+readr::write_tsv(x = globalvivid_run_map, 
+                 path = "~/Documents/MountPoints/mountedMeshnick/Projects/VivID_Seq/wgs_pe_improved_global/globalvivid_run_map.tab.txt",
+                 col_names = F)  
+
+
 
 
 

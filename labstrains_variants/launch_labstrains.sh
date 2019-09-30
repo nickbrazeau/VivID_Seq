24e2bbd94a7cfd46fbc7b4fb02a2c6e244f9d3b5
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-09-30 22:53:55 +0100

    get labstrains work

--- labstrains_variants/launch_labstrains.sh
+++ labstrains_variants/launch_labstrains.sh
@@ -1,18 +1,15 @@
 #! /bin/bash
 
-ROOT=/proj/ideel/meshnick/users/NickB/Projects/VivID_Seq/raw_variants
-WD=/pine/scr/n/f/nfb/Projects/VivID_Seq/vcfs_gatk_joint_raw/
-NODES=1028 # max number of cluster nodes
+ROOT=/proj/ideel/meshnick/users/NickB/Projects/VivID_Seq/labstrains_variants
+WD=/pine/scr/n/f/nfb/Projects/VivID_Seq/vcfs_gatk_lab_strains/
 WAIT=30 # number of seconds to wait for files to appear, absorbing some file system latency
 
 snakemake \
-	--snakefile $ROOT/call_gatk4.snake \
+	--snakefile $ROOT/get_labstrains.snake \
 	--configfile $ROOT/config.yaml \
 	--printshellcmds \
 	--directory $WD \
-	--cluster $ROOT/launch.py \
-	-j $NODES \
 	--rerun-incomplete \
 	--keep-going \
 	--latency-wait $WAIT \
-#	--dryrun -p
+	--dryrun -p

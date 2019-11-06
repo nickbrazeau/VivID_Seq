115d9965394d95a51daa98f2eb49730fccfe69b9
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-11-06 09:40:31 +0000

    typo for ebro

--- SpainEbro1944/ebro1944_varcaller.sh
+++ SpainEbro1944/ebro1944_varcaller.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-outdir=/pine/scr/n/f/nfb/Projects/VivID_Seq/Erbo1944
+outdir=/pine/scr/n/f/nfb/Projects/VivID_Seq/Ebro1944
 smpl=/pine/scr/n/f/nfb/Projects/VivID_Seq/wgs_se_improved_global/aln/merged/SRS1607662.bam
 region=/proj/ideel/meshnick/users/NickB/Projects/VivID_Seq/regions/mtdna.bed
 ref=/proj/ideel/resources/genomes/Pvivax/genomes/PvP01.fasta
@@ -8,4 +8,4 @@ ref=/proj/ideel/resources/genomes/Pvivax/genomes/PvP01.fasta
 mkdir -p $outdir
 
 bcftools mpileup $smpl -R $region  --fasta-ref $ref -Ou | \
-bcftools call -c  --variants-only > $outdir/Erbo1944.vcf
+bcftools call -c  --variants-only > $outdir/Ebro1944.vcf

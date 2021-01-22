b9ad9bea543aec06e185992bbb2c1842213087a5
nickbrazeau <nbrazeau@med.unc.edu>
2021-01-22 11:47:46 -0500

    get trees backend for longer bs

---
+++ PopGenome_Analysis/analyses/00-final_streamlined_analyses/get_trees_onLL.sbatch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --time=5-00:00:00
+#SBATCH --mem=49512
+#SBTACH --job-name=gettrees
+#SBATCH --mail-type=all
+#SBATCH --mail-user=nbrazeau@med.unc.edu
+
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/VivID_Seq/PopGenome_Analysis"); source("analyses/00-final_streamlined_analyses/fit_tree_backend.R")'

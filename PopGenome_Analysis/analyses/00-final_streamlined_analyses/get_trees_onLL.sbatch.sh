502a9062b00d92c807b3e0c2a86ff131dccff940
nickbrazeau <nbrazeau@med.unc.edu>
2021-01-22 11:54:25 -0500

    less mem for trees

--- PopGenome_Analysis/analyses/00-final_streamlined_analyses/get_trees_onLL.sbatch.sh
+++ PopGenome_Analysis/analyses/00-final_streamlined_analyses/get_trees_onLL.sbatch.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --ntasks=1
 #SBATCH --time=5-00:00:00
-#SBATCH --mem=49512
+#SBATCH --mem=24g
 #SBTACH --job-name=gettrees
 #SBATCH --mail-type=all
 #SBATCH --mail-user=nbrazeau@med.unc.edu
